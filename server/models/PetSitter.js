bbccca84e2b2b5169bc19b325660cc785073c10d
MUGABA <mugabamuha@gmail.com>
2022-01-26 16:49:28 +0300

    worked on pet sitter schema with proposed discriminator

---
+++ server/models/PetSitter.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Profile = require("./Profile");
+
+const petSitterSchema = new mongoose.Schema({
+  stripeConnectId: {
+    type: String,
+    required: true,
+  },
+  availabilityId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Availability",
+  },
+  activatedAvailabilitySchedule: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Schedule",
+  },
+  rate: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = PetSitter = Profile.discriminator(
+  "PetSitter",
+  petSitterSchema
+);
--- server/models/Profile.js
+++ server/models/Profile.js
@@ -4,7 +4,7 @@ const profileSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: 'User'
+    ref: "User",
   },
   name: {
     type: String,
@@ -16,7 +16,7 @@ const profileSchema = new mongoose.Schema({
   },
   gender: {
     type: String,
-    enum: ['male', 'female', 'other'],
+    enum: ["male", "female", "other"],
   },
   address: {
     type: String,
@@ -28,12 +28,13 @@ const profileSchema = new mongoose.Schema({
   },
   birthday: {
     type: Date,
-    default: null
+    default: null,
   },
   photo: {
     type: String,
     default: "",
   },
+  discriminatorKey: "userType",
 });
 
 module.exports = Profile = mongoose.model("Profile", profileSchema);
