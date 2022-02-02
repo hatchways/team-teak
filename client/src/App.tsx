b995af4e7f664f72cf1c6c44bb05957e9e80cec9
Robert Chen <wiroger9595@gmail.com>
2022-02-02 08:20:12 +0800

    add profile detail page feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ import { SnackBarProvider } from './context/useSnackbarContext';
 import { Navbar } from './components/Navbar/Navbar';
 import Settings from './pages/Settings/Settings';
 import NotFound from './pages/NotFound/NotFound';
+import ProfileDetails from './pages/Settings/ProfileDetails/ProfileDetails';
 
 function App(): JSX.Element {
   return (
@@ -29,6 +30,7 @@ function App(): JSX.Element {
                 <Route exact path="/signup" component={Signup} />
                 <Route exact path="/dashboard" component={Dashboard} />
                 <Route path="/profile/settings" component={Settings} />
+                <Route path="/profile/:profile-id" component={ProfileDetails} />
                 <Route path="*">
                   <NotFound />
                 </Route>
