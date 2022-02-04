7ea51477b6110fb369d0322dacec0567edd65720
Jacob Tingling <jacob.tingling@protonmail.com>
2022-02-04 16:16:59 -0500

    Typos with dummy data

--- client/src/components/ProfileListing/constants.ts
+++ client/src/components/ProfileListing/constants.ts
@@ -1,13 +1,13 @@
 export default [
   {
-    maintitle: 'Norma byers',
+    maintitle: 'Norma Byers',
     price: '$14/hr',
     title: 'Loving pet sitter',
-    description: 'Dog sitting,cat sitting,pocket pet and bird core',
+    description: 'Dog sitting, cat sitting, pocket pet and bird care',
     avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Keurig_Logo.png',
     imageUrl:
       'https://media.istockphoto.com/photos/studio-portrait-of-smiling-young-woman-holding-affectionate-pet-picture-id1170668075?k=20&m=1170668075&s=612x612&w=0&h=9BDN9UNU8D2o-dpLh_-Ow4SyCQy8L3B3eiz-F6JNpdo=',
-    country: 'Toronto,Ontario',
+    country: 'Toronto, Ontario',
   },
   {
     maintitle: 'Micheal Carnahan',
@@ -17,7 +17,7 @@ export default [
     avatarUrl: 'https://images-na.ssl-images-amazon.com/images/I/81CbxNLCHCL._AC_SL1500_.jpg',
     imageUrl:
       'https://media.istockphoto.com/photos/happy-young-beautiful-woman-hugging-her-favourite-pet-picture-id1181597935?k=20&m=1181597935&s=612x612&w=0&h=Pte0UIzAHA6B-BvLvAdt9b1WS4LEU469uz0k5qE0ezw=',
-    country: 'Toronto,Ontario',
+    country: 'Toronto, Ontario',
   },
   {
     maintitle: 'Jessica Pearson',
@@ -27,7 +27,7 @@ export default [
     avatarUrl: 'https://images-na.ssl-images-amazon.com/images/I/81mjh5ZSu1L._AC_SL1500_.jpg',
     imageUrl:
       'https://media.istockphoto.com/photos/young-woman-and-her-cute-puppy-of-cocker-spaniel-outdoors-picture-id1204670115?k=20&m=1204670115&s=612x612&w=0&h=zynSgnSes68lb_VA6sjjvp-eULmF1f3mbW9s_RTc2uA=',
-    country: 'Toronto,Ontario',
+    country: 'Toronto, Ontario',
   },
   {
     maintitle: 'Charles Compton',
@@ -37,7 +37,7 @@ export default [
     avatarUrl: 'https://images-na.ssl-images-amazon.com/images/I/61u76aiQQsL._AC_SL1000_.jpg',
     imageUrl:
       'https://media.istockphoto.com/photos/studio-portrait-of-smiling-young-woman-holding-little-dog-picture-id1311526307?k=20&m=1311526307&s=612x612&w=0&h=y-JLMkflmcJJb0pTy5xzFZ8pCLqCs5a3X7JvRVlMnKk=',
-    country: 'Toronto,Ontario',
+    country: 'Toronto, Ontario',
   },
   {
     maintitle: 'Charlotte Butler',
@@ -47,7 +47,7 @@ export default [
     avatarUrl: 'https://images-na.ssl-images-amazon.com/images/I/81BdxpiRtCL._AC_SL1500_.jpg',
     imageUrl:
       'https://media.istockphoto.com/photos/woman-in-knitted-sweater-with-her-maltipoo-poodle-picture-id1266979016?k=20&m=1266979016&s=612x612&w=0&h=LuLW6lcVQuXeLWCBd_8PXZuflETnQqZo4E9poI1QIzg=',
-    country: 'Toronto,Ontario',
+    country: 'Toronto, Ontario',
   },
   {
     maintitle: 'Joan Blakeney',
@@ -57,6 +57,6 @@ export default [
     avatarUrl: 'https://images-na.ssl-images-amazon.com/images/I/61PGHBLkl8L._AC_SL1500_.jpg',
     imageUrl:
       'https://media.istockphoto.com/photos/happy-pretty-woman-embracing-yorkshire-terrier-picture-id923578330?k=20&m=923578330&s=612x612&w=0&h=c-YI7vajMInzXmVUcjY0Sg1fQhF1cXdb4t_YEiJeC8M=',
-    country: 'Toronto,Ontario',
+    country: 'Toronto, Ontario',
   },
 ];
