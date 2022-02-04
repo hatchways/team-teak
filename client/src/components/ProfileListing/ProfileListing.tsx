ec84f6b7e68645e23cc203e51dddc33fe9aedd5b
Jacob Tingling <jacob.tingling@protonmail.com>
2022-02-04 17:08:49 -0500

    More styling and added default stars for rating

--- client/src/components/ProfileListing/ProfileListing.tsx
+++ client/src/components/ProfileListing/ProfileListing.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Box, Grid, Card, CardMedia, CardContent, Typography, CardHeader } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import StarIcon from '@mui/icons-material/Star';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 import { useStyles } from './useStyles';
 import staticData from './constants';
 
@@ -44,6 +46,25 @@ const ProfileListing = (): JSX.Element => {
                   <Typography sx={{ fontWeight: 300, fontSize: '12px' }} variant="h6">
                     {sitter.title}
                   </Typography>
+                  <Grid container justifyContent="center" alignItems="center">
+                    <Grid
+                      item
+                      sx={{
+                        '.MuiSvgIcon-root': {
+                          color: '#dec62f',
+                          fontSize: '20px',
+                          marginTop: '10px',
+                          marginBottom: '10px',
+                        },
+                      }}
+                    >
+                      <StarIcon />
+                      <StarIcon />
+                      <StarIcon />
+                      <StarIcon />
+                      <StarBorderIcon />
+                    </Grid>
+                  </Grid>
                   <Typography
                     sx={{ fontWeight: 500, fontSize: '15px', width: '200px', margin: '0 auto' }}
                     variant="body1"
@@ -51,7 +72,12 @@ const ProfileListing = (): JSX.Element => {
                     {sitter.description}
                   </Typography>
                 </CardContent>
-                <CardContent>
+                <CardContent
+                  sx={{
+                    paddingLeft: '0',
+                    paddingRight: '0',
+                  }}
+                >
                   <Grid
                     container
                     alignItems="center"
@@ -59,9 +85,12 @@ const ProfileListing = (): JSX.Element => {
                     sx={{
                       gridTemplateColumns: '10% 65% 25%',
                       gridTemplateRows: '50px',
+                      borderTopStyle: 'solid',
+                      borderTopWidth: '1px',
+                      borderTopColor: 'rgba(0, 0, 0, 0.1)',
                     }}
                   >
-                    <Grid item>
+                    <Grid item justifySelf="center">
                       <LocationOnIcon className={classes.icon} />
                     </Grid>
                     <Grid item>
@@ -69,7 +98,7 @@ const ProfileListing = (): JSX.Element => {
                         {sitter.country}
                       </Typography>
                     </Grid>
-                    <Grid item sx={{ justifySelf: 'end' }}>
+                    <Grid item sx={{ justifySelf: 'center' }}>
                       <Typography variant="subtitle2" sx={{ fontWeight: 800, fontSize: '16px' }}>
                         {sitter.price}
                       </Typography>
