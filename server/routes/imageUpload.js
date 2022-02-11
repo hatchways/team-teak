78b5032b8969e75f698036c10b9c3c14a8d8ed10
Robert Chen <wiroger9595@gmail.com>
2022-02-11 23:57:02 +0800

    delete multerImageUpload

--- server/routes/imageUpload.js
+++ server/routes/imageUpload.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { multerImageUpload } = require("../middleware/multer");
-
 
 const protect = require('../middleware/auth');
 const { uploadPicture } = require('../controllers/imageUpload')
 
-router.route("/upload").post(multerImageUpload, protect, uploadPicture );
+router.route("/upload").post(protect, uploadPicture );
 
 module.exports = router; 
