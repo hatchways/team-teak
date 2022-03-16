7ed87722d148b1d32b8adb555da1547a7a2c7790
Robert Chen <wiroger9595@gmail.com>
2022-03-16 22:42:22 +0800

    no message

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,40 +11,45 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 // @access Public
 exports.registerUser = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.body;
-
+  
   const emailExists = await User.findOne({ email });
-
+  
   if (emailExists) {
     res.status(400);
     throw new Error("A user with that email already exists");
   }
-
+  
   const userExists = await User.findOne({ name });
-
+  
   if (userExists) {
     res.status(400);
     throw new Error("A user with that username already exists");
   }
-
+  
   const user = await User.create({
     name,
     email,
     password,
   });
-
+  
   const isPetSitter = req.query.accountType === "petSitter" ? true : false;
-
+  
   if (user) {
+    const customer = await stripe.customers.create({
+      description: `Customer name is ${name}`,
+    });
+    const { id } = customer;
     if (isPetSitter) {
       await PetSitter.create({
         userId: user._id,
+        stripeConnectId: id,
         name,
       });
     } else {
       const customer = await stripe.customers.create({
         description: `Customer name is ${name}`,
       });
-
+  
       const { id } = customer;
       const profile = await Profile.create({
         userId: user._id,
@@ -52,15 +57,14 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
         name,
       });
     }
-
     const token = generateToken(user._id);
     const secondsInWeek = 604800;
-
+  
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: secondsInWeek * 1000,
     });
-
+  
     res.status(201).json({
       success: {
         user: {
@@ -74,46 +78,43 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
     res.status(400);
     throw new Error("Invalid user data");
   }
-});
-
-// @route POST /auth/login
-// @desc Login user
-// @access Public
-exports.loginUser = asyncHandler(async (req, res, next) => {
-  // const isDemo = req.query.isDemo;
-
+ });
+  
+ // @route POST /auth/login
+ // @desc Login user
+ // @access Public
+ exports.loginUser = asyncHandler(async (req, res, next) => {
+  const isDemo = req.query.isDemo;
+  
   let email, password;
-
-  // if (!isDemo) {
-  //   email = req.body.email;
-  //   password = req.body.password;
-  // } else {
-  //   email = process.env.DEMO_USER_EMAIL;
-  //   password = process.env.DEMO_USER_PASSWORD;
-  // }
-
-  const user = await User.findOne({ email: req.body.email });
-
+  
+  if (!isDemo) {
+    email = req.body.email;
+    password = req.body.password;
+  } else {
+    email = process.env.DEMO_USER_EMAIL;
+    password = process.env.DEMO_USER_PASSWORD;
+  }
+  
+  const user = await User.findOne({ email });
+  const userId = user.id;
+  
   if (!user) {
     res.status(400);
     throw new Error("Wrong email or password!");
   }
-  const profile = await Profile.findOne({ userId: user.id });
   const notifications = await Notification.find({ recieverId: user.id });
-
-  profile.set({ isOnline: true });
-
-  const updatedProfile = await profile.save();
-
+  const profile = await Profile.findOneAndUpdate(userId, { isOnline: true });
+  
   if (user && (await user.matchPassword(password))) {
     const token = generateToken(user._id);
     const secondsInWeek = 604800;
-
+  
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: secondsInWeek * 1000,
     });
-
+  
     res.status(200).json({
       success: {
         user: {
@@ -129,7 +130,8 @@ exports.loginUser = asyncHandler(async (req, res, next) => {
     res.status(401);
     throw new Error("Invalid email or password");
   }
-});
+ });
+ 
 
 // @route GET /auth/user
 // @desc Get user data with valid token
